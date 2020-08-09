836d6ceb7cc197143f1a394da4aa5b918820b6f8
unknown <andersonlantunes@gmail.com>
2020-08-08 21:22:09 -0300

    Arquivo com dados de teste

---
+++ src/database/fake_data.js
@@ -0,0 +1,28 @@
+const proffys = [
+  {
+    name: "Anderson Antunes",
+    avatar: `https://avatars1.githubusercontent.com/u/48885210?
+        s=460&u=5b39afe2f6e5c704ef722dae12994ddf04500701&v=4`,
+    whatsapp: "11974378859",
+    bio:
+      "Entusiasta das melhores tecnologias de química avançada.<br /><br /> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    name: "Luiz Antunes",
+    avatar:
+      "https://avatars1.githubusercontent.com/u/48885210?s=460&u=5b39afe2f6e5c704ef722dae12994ddf04500701&v=4",
+    whatsapp: "11974378859",
+    bio:
+      "Entusiasta das melhores tecnologias de química avançada.<br /><br /> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+  },
+];
