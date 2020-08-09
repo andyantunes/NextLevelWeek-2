f823070a342cf6cc4e8c0f8bc8d74f97d23228d5
unknown <andersonlantunes@gmail.com>
2020-08-08 22:34:12 -0300

    Finalizado query de proffys

--- src/pages.js
+++ src/pages.js
@@ -6,6 +6,7 @@ const {
   getSubject,
   convertHoursToMinutes,
 } = require("./utils/format");
+const createProffy = require("./database/createProffy");
 
 function pageLanding(require, response) {
   return response.render("index.html");
@@ -39,6 +40,10 @@ async function pageStudy(require, response) {
     const db = await Database;
     const proffys = await db.all(query);
 
+    proffys.map((proffy) => {
+      proffy.subject = getSubject(proffy.subject);
+    });
+
     return response.render("study.html", {
       proffys,
       subjects,
@@ -51,20 +56,44 @@ async function pageStudy(require, response) {
 }
 
 function pageGiveClasses(require, response) {
-  const data = require.body;
-
-  const isNotEmpty = Object.keys(data).length > 0;
-
-  if (isNotEmpty) {
-    data.subject = getSubject(data.subject);
-    proffys.push(data);
-
-    return response.redirect("/study");
-  }
-
   return response.render("give-classes.html", { subjects, weekdays });
 }
 
-function saveClasses(require, response) {}
+async function saveClasses(req, resp) {
+  const createProffy = require("./database/createProffy");
+
+  const proffyValue = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+    whatsapp: req.body.whatsapp,
+    bio: req.body.bio,
+  };
+
+  const classValue = {
+    subject: req.body.subject,
+    cost: req.body.cost,
+  };
+
+  const classScheduleValues = req.body.weekday.map((weekday, index) => {
+    return {
+      weekday,
+      time_from: convertHoursToMinutes(req.body.time_from[index]),
+      time_to: convertHoursToMinutes(req.body.time_to[index]),
+    };
+  });
+
+  try {
+    const db = await Database;
+    await createProffy(db, { proffyValue, classValue, classScheduleValues });
+
+    let queryString = "?subject=" + req.body.subject;
+    queryString += "&weekday=" + req.body.weekday[0];
+    queryString += "&time=" + req.body.time_from[0];
+
+    return resp.redirect("/study" + queryString);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = { pageLanding, pageStudy, pageGiveClasses, saveClasses };
